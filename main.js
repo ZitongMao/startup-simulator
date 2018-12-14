44ecfcb45d5a0895c6daf0bfd603532a59619528
Zitong Mao <njsxmzt@gmail.com>
2018-12-14 15:08:20 +0900

    Change Colors of In-Text Data Automatically

--- main.js
+++ main.js
@@ -39,23 +39,23 @@ let questions = [
         oddsB : 0.2,
         oddsC : 0.9,
 
-        resultA : "You product has very good synergies with Facehook. <br><br> Valuation + 100M <br> Cash + 30M <br> Morale - 1",
+        resultA : "You product has very good synergies with Facehook. <br><br> @Valuation + 100M <br> Cash + 30M@ <br> %Morale - 1%",
         // data [valuation, cash, morale]
         dataA : [100, 30, -1],
 
         resultA_bad : "GAME OVER <br> <br> You are asked to step down as the CEO and you no longer have control over your company. <br> Good thing is that you are now pretty wealthy.",
         dataA_bad: [0, 0, -10],
 
-        resultB : "Facehook's CEO is angry. They created an exact copy of your product. However, customers love your product more. <br><br> Valuation + 50M <br> Cash + 10M <br> Morale + 3",
+        resultB : "Facehook's CEO is angry. They created an exact copy of your product. However, customers love your product more. <br><br> @Valuation + 50M <br> Cash + 10M <br> Morale + 3@",
         dataB: [50, -10, 3],
 
-        resultB_bad : "Facehook's CEO is angry. They created an exact copy of your product and made all of their users signed up. <br><br> Valuation - 50M <br> Cash - 10M <br> Morale + 1",
+        resultB_bad : "Facehook's CEO is angry. They created an exact copy of your product and made all of their users sign up. <br><br> %Valuation - 50M <br> Cash - 10M% <br> @Morale + 1@",
         dataB_bad: [-50, -10, 1],
 
-        resultC : "The negotiation is successful. <br><br> Valuation + 100 <br> Cash + 30 <br> Morale + 2",
+        resultC : "The negotiation is successful. <br><br> @Valuation + 100 <br> Cash + 30 <br> Morale + 2@",
         dataC: [100, 30, 2],
 
-        resultC_bad : "The negotiation is unsuccessful. Facehook's CEO is angry. They created an exact copy of your product and made all of their users signed up. <br><br> Valuation - 20M <br> Morale - 1",
+        resultC_bad : "The negotiation is unsuccessful. Facehook's CEO is angry. They created an exact copy of your product and made all of their users signed up. <br><br> %Valuation - 20M <br> Morale - 1%",
         dataC_bad: [-20, 0, -1],
 
     },{
@@ -71,20 +71,20 @@ let questions = [
         oddsB : 0.3,
         oddsC : 0.9,
 
-        resultA : "Your startup grew significantly with this extra cash. <br><br> Valuation + 800M <br> Cash + 200M <br> Morale +5",
+        resultA : "Your startup grew significantly with this extra cash. <br><br> @Valuation + 800M <br> Cash + 200M <br> Morale +5@",
         // data [valuation, cash, morale]
         dataA : [800, 200, 5],
 
-        resultA_bad : "SoftTank asked you to quit all the international markets because they also invested in competitor companies in those countries.<br> <br> Valuation + 100 <br> Cash + 50 <br> Morale - 5",
+        resultA_bad : "SoftTank asked you to quit all the international markets because they also invested in competitor companies in those countries.<br> <br> @Valuation + 100 <br> Cash + 50@ <br> %Morale - 5%",
         dataA_bad: [100, 50, -5],
 
-        resultB : "Things are going well, and your company becomes famous. <br><br> Valuation + 300M <br> Cash + 50M <br> Morale + 5",
+        resultB : "Things are going well, and your company becomes famous. <br><br> @Valuation + 300M <br> Cash + 50M <br> Morale + 5@",
         dataB: [300, 50, 5],
 
-        resultB_bad : "SoftTank invested your competitor. <br><br> Valuation - 200M <br> Cash - 10M <br> Morale -3",
+        resultB_bad : "SoftTank invested your competitor. <br><br> %Valuation - 200M <br> Cash - 10M <br> Morale -3%",
         dataB_bad: [-200, -10, -3],
 
-        resultC : "The negotiation is successful. <br><br> Valuation + 100 <br> Cash + 30 <br> Morale + 2",
+        resultC : "The negotiation is successful. <br><br> @Valuation + 100 <br> Cash + 30 <br> Morale + 2@",
         dataC: [100, 30, 2],
 
         resultC_bad : "The negotiation is unsuccessful. Facehook's CEO is angry. They created an exact copy of your product and made all of their users signed up. <br><br> Valuation - 20M <br> Morale - 1",
@@ -319,9 +319,16 @@ function randomResult(odds){
         // bad result
         return false;
     }
-
 }
 
+// color data change, positive to green, negative to red
+function changeTextColor(str){
+    // negative
+    str=str.replace(/(%)(.*)(%)/g,"<font color='red'>$2</font>");
+    // positive
+    str=str.replace(/(@)(.*)(@)/g,"<font color='green'>$2</font>");
+    return str
+}
 
 
 // render starting stats
@@ -363,14 +370,14 @@ function renderResult(choice){
     let q = questions[runningQuestion];
     if (choice === 'A') {
         if (randomResult(q.oddsA)){
-            resultDescription.innerHTML = q.resultA;
+            resultDescription.innerHTML = changeTextColor(q.resultA);
             valuation += q.dataA[0];
             cash += q.dataA[1];
             morale += q.dataA[2];
             time++;
             renderStats();
         } else {
-            resultDescription.innerHTML = q.resultA_bad;
+            resultDescription.innerHTML = changeTextColor(q.resultA_bad);
             valuation += q.dataA_bad[0];
             cash += q.dataA_bad[1];
             morale += q.dataA_bad[2];
@@ -380,14 +387,14 @@ function renderResult(choice){
 
     } else if (choice === 'B') {
         if (randomResult(q.oddsB)){
-            resultDescription.innerHTML = q.resultB;
+            resultDescription.innerHTML = changeTextColor(q.resultB);
             valuation += q.dataB[0];
             cash += q.dataB[1];
             morale += q.dataB[2];
             time++;
             renderStats();
         } else {
-            resultDescription.innerHTML = q.resultB_bad;
+            resultDescription.innerHTML = changeTextColor(q.resultB_bad);
             valuation += q.dataB_bad[0];
             cash += q.dataB_bad[1];
             morale += q.dataB_bad[2];
@@ -396,14 +403,14 @@ function renderResult(choice){
         }
     } else if (choice === 'C') {
         if (randomResult(q.oddsC)){
-            resultDescription.innerHTML = q.resultC;
+            resultDescription.innerHTML = changeTextColor(q.resultC);
             valuation += q.dataC[0];
             cash += q.dataC[1];
             morale += q.dataC[2];
             time++;
             renderStats();
         } else {
-            resultDescription.innerHTML = q.resultC_bad;
+            resultDescription.innerHTML = changeTextColor(q.resultC_bad);
             valuation += q.dataC_bad[0];
             cash += q.dataC_bad[1];
             morale += q.dataC_bad[2];
